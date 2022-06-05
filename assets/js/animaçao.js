ff1402c4523b4a35cf817b854168ee16c0bd3f92
rafael16k <leafar.aureliano@gmail.com>
2022-06-05 05:09:35 -0300

    portfolio

---
+++ assets/js/animaçao.js
@@ -0,0 +1,91 @@
+const home = document.querySelector('#home')
+const arrowUp = document.querySelector('#arrow-up a ')
+const homeHeight = home.offsetHeight
+function changeHomeWhenScroll() {
+  if (window.scrollY >= homeHeight) {
+    //scroll é maior que a altura do header
+    arrowUp.classList.add('scroll')
+  } else {
+    //menor que a altura do header
+    arrowUp.classList.remove('scroll')
+  }
+}
+
+
+window.addEventListener('scroll', function () {
+  changeHomeWhenScroll()
+})
+
+
+/* ScrollReveal */
+
+//TOP
+const scrollReveal = ScrollReveal({
+  origin: 'top',
+  distance: '30px',
+  duration: 700,
+  reset: true
+})
+
+scrollReveal.reveal(
+  `#header .container,
+  
+   #about .bloco-1--about,
+   #services .bloco-services,
+   #portfolio .projetos,
+   #contato .container
+  `,
+
+  { interval: 200 }
+)
+//BOTTOM
+const scrollRevealBottom = ScrollReveal({
+  origin: 'bottom',
+  distance: '60px',
+  duration: 700,
+  reset: true
+})
+
+scrollRevealBottom.reveal(
+  `
+   #resol-code .wrapper,
+   #depoimento .blocos-depoimentos, #depoimento .blocos-depoimentos
+   
+  
+  `,
+
+  { interval: 200 }
+)
+
+// LEFT
+const scrollRevealLeft = ScrollReveal({
+  origin: 'left',
+  distance: '30px',
+  duration: 700,
+  reset: true
+})
+
+scrollRevealLeft.reveal(
+  `#home .bloco-1
+ 
+  `,
+
+  { interval: 200 }
+)
+
+//RIGHT
+const scrollRevealRight = ScrollReveal({
+  origin: 'right',
+  distance: '30px',
+  duration: 700,
+  reset: true
+})
+
+scrollRevealRight.reveal(
+  `#home .bloco-3,
+   #about .bloco-2--about
+ 
+  `,
+
+  { interval: 200 }
+)
